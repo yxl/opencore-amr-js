00dbf47934b034a61a3796bc17bfba1c290013a6
Martin Storsjo <martin@martin.st>
2009-08-07 02:19:36 +0300

    Changed the build_osx.sh script to use the old static makefiles (until updated)

--- build_osx.sh
+++ build_osx.sh
@@ -5,6 +5,6 @@ export CXXFLAGS="$CFLAGS"
 export LDFLAGS="-Wl,-syslibroot,/Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.4 -arch ppc -arch i386"
 export BUILD_AS_C=1
 
-make -C amrnb "$@" || exit 1
-make -C amrwb "$@" || exit 1
+make -f Makefile.alt -C amrnb "$@" || exit 1
+make -f Makefile.alt -C amrwb "$@" || exit 1
 
